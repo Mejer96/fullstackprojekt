35e1494264ae01b2dd8b7c0710e6baf4d9338583
Mathiasrhansen <math86s0@stud.kea.dk>
2022-03-29 14:28:47 +0200

    Så vi igang 2

---
+++ src/main/java/com/example/projekt2/controllers/indexController.java
@@ -0,0 +1,14 @@
+package com.example.projekt2.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class indexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
