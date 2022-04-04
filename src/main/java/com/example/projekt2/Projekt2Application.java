15bfaa91148880c811b4d1f9e4ded2e9cd2d3737
mejer96 <carlemilfalkmejer96@gmail.com>
2022-04-04 06:22:17 +0200

    Tilføjet, user, wish, wishlist klasser. Tilføjet en række get og post metoder til controller til at oprette og vise wish, brugere, wishlist (mangler dog frontend) Repository klasse med forbindelse til MySQL database, samt et par metoder med MySQL statements. Startet på service klasse

--- src/main/java/com/example/projekt2/Projekt2Application.java
+++ src/main/java/com/example/projekt2/Projekt2Application.java
@@ -3,11 +3,12 @@ package com.example.projekt2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 @SpringBootApplication
 public class Projekt2Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Projekt2Application.class, args);
     }
-
 }
